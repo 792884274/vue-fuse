68de8b0ec04a88bf53b64686e509bc77a495b9aa
792884274 <792884274@qq.com>
2018-01-27 10:48:31 +0800

    update

--- config/index.js
+++ config/index.js
@@ -55,7 +55,8 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    // assetsPublicPath: './',
+    assetsPublicPath: 'vue-fuse',
 
     /**
      * Source Maps
