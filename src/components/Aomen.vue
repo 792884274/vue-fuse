34c0bb9862eb758132657a6491062947a1a94d22
792884274 <792884274@qq.com>
2018-01-27 17:53:35 +0800

    update

--- src/components/Aomen.vue
+++ src/components/Aomen.vue
@@ -127,6 +127,105 @@ before(app) {
 						<li>4.<code>getters</code>获取状态集的数据。</li>
 					</ul>
 				</li>
+				<li id="nginx">
+					<h5 id="-" class="point-header">项目上传到nginx服务器上，公网访问</h5>
+					<ul>
+						<li><code>vue</code>项目上传到<code>nginx</code>服务器上，公网访问</li>
+						<li>1.项目完成后<code>npm run build</code>，生成dist目录。<blockquote>
+							<p>线上访问地址为<a href="http://792884274.com/vue-fuse/">http://792884274.com/vue-fuse/</a> ,所以在router/index.js中设置：</p>
+						</blockquote>
+						</li>
+							</ul>
+							<div class="vs js">
+							<pre><code>
+export default new Router({
+  mode: &#39;history&#39;,
+  base: &#39;/vue-fuse/&#39;,
+  routes: [
+    ...
+  ]
+})
+</code></pre>
+							</div>
+					<ul>
+						<li>2.<code>github</code>托管项目。</li>
+						<li>3.<code>Xshell</code>命令行中，中将项目clone到服务器上指定文件夹中。
+							<pre><code>
+[root@izhp37e37j73ghn6yfwf7qz app]# git clone https://github.com/792884274/vue-fuse.git
+</code></pre>				<blockquote>
+							<p>同样可以用压缩项目，上传压缩文件(<code>rz -bey 压缩文件</code>)，解压项目压缩文件(<code>unzip 压缩文件</code>)的方式上传到服务器。</p>
+						</blockquote>
+						</li>
+						<li>4.在nginx的html文件夹下创建项目软链接
+							<pre><code>
+[root@izhp37e37j73ghn6yfwf7qz ~]# cd /usr/local/nginx/html
+[root@izhp37e37j73ghn6yfwf7qz html]# ln -s ~/app/vue-fuse/dist /usr/local/nginx/html/vue-fuse
+</code></pre>  
+						<blockquote>
+							<p>同样可以将项目直接放到<code>nginx</code>的html文件夹下，如此就如需在创建软链接。</p>
+						</blockquote>
+						</li>
+						<li>5.<code>nginx.conf</code>配置
+							<div class="vs js">
+								<pre><code>
+http{
+  server {
+    listen 80;
+    server_name 792884274.com www.792884274.com;
+    access_log  logs/792884274.log;
+    location /vue-fuse/ {
+      root  html;
+      index  index.html index.htm;
+    }
+  }
+}
+</code></pre>			
+						</div>
+						</li>
+						<li>6.重启服务
+							<pre><code>
+[root@izhp37e37j73ghn6yfwf7qz conf]# cd ../sbin
+[root@izhp37e37j73ghn6yfwf7qz sbin]# ./nginx -t
+nginx: the configuration file /usr/local/nginx/conf/nginx.conf syntax is ok
+nginx: configuration file /usr/local/nginx/conf/nginx.conf test is successful
+[root@izhp37e37j73ghn6yfwf7qz sbin]# ./nginx -s reload
+</code></pre>
+						</li>
+						<li>7.线上访问 <a href="http://792884274.com/vue-fuse/">http://792884274.com/vue-fuse/</a> 。</li>
+					</ul>
+				</li>
+				<li id="url">
+					<h5 id="-" class="point-header">去除地址栏中/#/</h5>
+					路由配置中设置<code>mode</code>模式为<code>history</code>。
+					<div class="vs js">
+						<pre><code>
+export default new Router({
+  mode: &#39;history&#39;,
+  ...
+})
+</code></pre>
+					</div>
+					<p class="margin-top-0">上线后地址栏不再显示<code>/#/</code>。</p>
+					<blockquote>
+					<p><code>vue-router</code>默认 hash 模式 —— 使用 URL 的 hash 来模拟一个完整的 URL，于是当 URL 改变时，页面不会重新加载。</p>
+					</blockquote>
+					<p>当F5重新刷新页面的时候出现404，这时需要配置<code>nginx.conf</code>。</p>
+					<div class="vs js">
+						<pre><code>
+http{
+  server {
+    ...
+    location /vue-fuse/ {
+      root  html;
+      try_files $uri $uri/ /vue-fuse/index.html?args;
+      index  index.html index.htm;
+    }
+  }
+}
+</code></pre>
+					</div>
+				</li>
+
 
 			</ul>
 			
